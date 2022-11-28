33a672c096c58a1d089d6cce183765311a042947
munokura <36838778+munokura@users.noreply.github.com>
2022-11-28 15:00:21 +0900

    バージョン表記修正
    バージョン表記修正

--- PH_Warehouse.js
+++ PH_Warehouse.js
@@ -3,9 +3,8 @@
  PH - Warehouse/Storage
  @plugindesc This plugin allows the creation of warehouses where you can store items in the game.
  @author PrimeHover
- @version 1.2.1
- @date 05/30/2016
-
+ @version 1.2.2
+ @date 2020/07/07
  --------------------------------------------------------------------------------
  This work is licensed under the Creative Commons Attribution 4.0 International License.
  To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/
